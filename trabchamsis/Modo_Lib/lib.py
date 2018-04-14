13c16240d332ec4e6021104f553a8eb763c3e22f
Rafael Figueira Goncalves <capitao.rafa@gmail.com>
2018-04-14 14:30:15 -0300

    Uploading from c9

---
+++ trabchamsis/Modo_Lib/lib.py
@@ -0,0 +1,12 @@
+from ../Modo_Nucleo import nucleo.py
+
+
+
+
+
+
+if __name__ == '__main__':
+    # isNucleo=True;
+    print 'Modo Nucleo:' ,isNucleo
+    print (ReadFromToPosition(2,5))
+    # WriteEndFile('zeros e uns')
---
+++ trabchamsis/Modo_Nucleo/nucleo.py
@@ -0,0 +1,21 @@
+fileName=".txt"
+isNucleo=False;
+
+
+def WriteEndFile(message):
+    file = open(fileName,"a")
+    file.write(message)
+
+def ReadFromToPosition(initial,final):
+    file = open(fileName,"r").read()
+    return file[initial:final]
+    
+
+# print file.read()
+
+
+if __name__ == '__main__':
+    # isNucleo=True;
+    # print 'Modo Nucleo:' ,isNucleo
+    # print (ReadFromToPosition(2,5))
+    # WriteEndFile('zeros e uns')
