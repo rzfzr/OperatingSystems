52981a971544bf578bda4d79e97de88c13057026
Rafael Figueira Goncalves <capitao.rafa@gmail.com>
2018-04-14 15:30:52 -0300

    Crashed

--- trabchamsis/Modo_Lib/lib.py
+++ trabchamsis/Modo_Lib/lib.py
@@ -1,10 +1,8 @@
 import os
 import sys
-sys.path.insert(0, os.getcwd())
-# sys.path.insert(0, '..')  # imports from parent folder
-# sys.path.insert(0, '.././Modo_Nucleo')
-# from Modo_Nucleo.nucleo import ReadFromToPosition
+sys.path.insert(0, os.getcwd())  # adds current dir to import from
 
+# from Modo_Nucleo.nucleo import ReadFromToPosition
 from Modo_Nucleo.nucleo import *
 
 if __name__ == '__main__':
