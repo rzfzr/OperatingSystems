a466502dff27537e3cc5e6efdce29bb7e429c66b
Rafael Figueira Goncalves <capitao.rafa@gmail.com>
2018-04-14 15:09:09 -0300

    Imports working

--- trabchamsis/Modo_Lib/lib.py
+++ trabchamsis/Modo_Lib/lib.py
@@ -1,12 +1,13 @@
-from ../Modo_Nucleo import nucleo.py
+import os
+import sys
+sys.path.insert(0, os.getcwd())
+# sys.path.insert(0, '..')  # imports from parent folder
+# sys.path.insert(0, '.././Modo_Nucleo')
+# from Modo_Nucleo.nucleo import ReadFromToPosition
 
-
-
-
-
+from Modo_Nucleo.nucleo import *
 
 if __name__ == '__main__':
-    # isNucleo=True;
-    print 'Modo Nucleo:' ,isNucleo
-    print (ReadFromToPosition(2,5))
+    print('Modo Nucleo:', isNucleo)
+    print(ReadFromToPosition(2, 5))
     # WriteEndFile('zeros e uns')
--- trabchamsis/Modo_Nucleo/nucleo.py
+++ trabchamsis/Modo_Nucleo/nucleo.py
@@ -1,21 +1,22 @@
-fileName=".txt"
-isNucleo=False;
+fileName = "Modo_Nucleo/binary.txt"
+isNucleo = False
 
 
 def WriteEndFile(message):
-    file = open(fileName,"a")
+    file = open(fileName, "a")
     file.write(message)
 
-def ReadFromToPosition(initial,final):
-    file = open(fileName,"r").read()
+
+def ReadFromToPosition(initial, final):
+    file = open(fileName, "r").read()
     return file[initial:final]
-    
+
 
 # print file.read()
 
 
-if __name__ == '__main__':
-    # isNucleo=True;
-    # print 'Modo Nucleo:' ,isNucleo
-    # print (ReadFromToPosition(2,5))
-    # WriteEndFile('zeros e uns')
+# if __name__ == '__main__':
+#     isNucleo = True
+#     print('Modo Nucleo:', isNucleo)
+#     # print(ReadFromToPosition(2, 5))
+#     WriteEndFile('zeros e uns')
