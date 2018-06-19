c1c043c7b0abdaa35253eb24daf9b91f4db9febb
Rafael Figueira Goncalves <capitao.rafa@gmail.com>
2018-06-19 17:35:32 -0300

    Finished dinner

--- dining_philosophers/dine.py
+++ dining_philosophers/dine.py
@@ -21,10 +21,12 @@ from threading import Thread
 import os
 import multiprocessing
 
-pool= multiprocessing.Pool(processes=(multiprocessing.cpu_count() - 2))
+# pool= multiprocessing.Pool(processes=(multiprocessing.cpu_count() - 2))
 
 seats = []
 forks = []
+tickets = []
+
 
 class State(Enum):
     THINKING = 0
@@ -40,50 +42,68 @@ class Fork():
         self.id = id
         self.inUse = False
 
-
 class Seat():
     phil = '' 
     def __init__(self,id):
         self.id = id
 
 class Phil():
-    def __init__(self , id , hunger):
+    def __init__(self , id , level):
         self.id = id
-        self.hunger = hunger
-        # self.level = level
+        # self.hunger = hunger
         self.state=State.THINKING
+
+        self.luck = 0
+        if level == Level.LOW:
+            self.hunger = 333
+        elif level == Level.MEDIUM:
+            self.hunger = 666
+        elif level == Level.HIGH:
+            self.hunger = 999
+
+
+        while self.luck > 0:
+            self.luck=-1
+            tickets.append(id)
         
     
 
 
 def Eat(choice,left,right):
-
-
     choice.phil.state=State.EATING
     forks[left].inUse = True
     forks[right].inUse = True
-
-
-    # if choice.phil.state == State.EATING:
-
-
-
-    choice.phil.hunger-=2
+    
     time.sleep(5)
+    choice.phil.hunger-=1
 
     choice.phil.state = State.THINKING
     forks[left].inUse = False
     forks[right].inUse = False
 
-# class Payload(object):
-#     def __init__(self, d):
-#         self.__dict__ = d
 
-
+def Lucky(tickets):
+    choice = ''
+    lucky = random.choice(tickets)
+    # print('random:',lucky)
+    
+    tickets = list(filter(lambda a: a != lucky, tickets))              # print('tickets:',tickets)
+    for seat in seats:
+        if seat.phil.id == lucky:
+            choice = seat
+            break
 
 def Waiter():
-    # while(True):
+
+    global tickets
+    
+    # if len(tickets) == 0:
+        # print('out of tickets')
+        # return
+
+
     choice = random.choice(seats)
+
     # print('choice:',choice.id) #,'position:',seats.index(choice))
     # print(choice.phil.state)
     # print('hunger:',choice.phil.hunger)
@@ -91,16 +111,21 @@ def Waiter():
     if choice.phil.hunger > 0:
         left,right =CheckForks(seats.index(choice))
         if not left and not right:
-            print('cannot eat!')
-            ammount = 10
+            # print('cannot eat!')
+            amount = 10
         else:
-            print('eat!')
+            # print('eat!')
             # ammount = 10
             # p = Payload(choice)
-            process = multiprocessing.Process(target=Eat, args=(choice,left,right,))
+
+
+
+            Thread(target=Eat, args=(choice,left,right,)).start()
+
+            # process = multiprocessing.Process(target=Eat, args=(choice,left,right,))
+            # process.start()
             
             #seats.index(choice),))
-            process.start()
 
 
             # Thread(target=Eat, args=(choice,)).start()
@@ -110,37 +135,70 @@ def Waiter():
         # print('not hungry!')
 
 
-    time.sleep(2)
+    time.sleep(.1)
     Waiter()
         
         
 def Map():
 
-    print('| 1 | 2 | 3 | 4 | 5 |')
-    print(
-        seats[0].phil.state,
-        seats[1].phil.state,
-        seats[2].phil.state,
-        seats[3].phil.state,
-        seats[4].phil.state)
+    
+
+    hunger = '|'
+    for seat in seats:
+        hunger += '  '+ str(seat.phil.hunger)+' |'
+
+    
+    ids = '|'
+    for seat in seats:
+        ids += '   '+str(seat.phil.id)+'  |'
+
+    states = '|'
+    for seat in seats:
+        if seat.phil.state==State.EATING:
+            states += '   E  |'
+        else:
+            states += '      |'
+    
+    message = '    '
+    for fork in forks:
+        if fork.inUse:
+            message += '       '
+        else:
+            message += '   Y   '
+
+
+    print(hunger)
+    print(ids)
+    print(states)
+    print(message)
+    
+    
     time.sleep(1)
     Map()
 
 def CheckForks(id):
+    # Map()
+
+    # print('id:',id)
+
     if id == 0:
         left = forks[-1] 
         right = forks[0]
     elif id == 4:
-        left = forks[id]
-        right = forks[0]
+        left = forks[id-1]
+        right = forks[id]
     else:
-        left = forks[id]
-        right = forks[id+1]
+        left = forks[id-1]
+        right = forks[id]
 
     if not left.inUse and not right.inUse:
+
+        # print('forks:',forks.index(left),' r:',forks.index(right))
         return forks.index(left), forks.index(right)
     else:
         return False, False
+
+
 
 
 if __name__ == '__main__':
@@ -157,11 +215,11 @@ if __name__ == '__main__':
     seats.append(Seat(7))
     seats.append(Seat(9))
     
-    seats[0].phil = Phil(0,10)
-    seats[1].phil = Phil(1,10)
-    seats[2].phil = Phil(2,10)
-    seats[3].phil = Phil(3,10)
-    seats[4].phil = Phil(4,10)
+    seats[0].phil = Phil(0,Level.LOW)
+    seats[1].phil = Phil(1,Level.LOW)
+    seats[2].phil = Phil(2,Level.MEDIUM)
+    seats[3].phil = Phil(3,Level.HIGH)
+    seats[4].phil = Phil(4,Level.HIGH)
 
     # process = multiprocessing.Process(target=Map, args=())
     # process.start()
